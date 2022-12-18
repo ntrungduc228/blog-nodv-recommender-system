3773c56af545af16d5340d34bd02f5deda339a6e
Duc Nguyen <ntrungduc228@gmail.com>
2022-12-19 02:02:13 +0700

    training data

--- src/app.py
+++ src/app.py
@@ -1,14 +1,14 @@
-from flask import Flask,jsonify
+from flask import Flask, jsonify
 from flask_restful import Resource, Api
 import numpy as np
 import pymongo
-# importing ObjectId from bson library
 from bson.objectid import ObjectId
 import json
-from pyeditorjs import EditorJsParser
 
 import settings
-from utils import editorJsDataToText
+from src.distances import get_most_similar_documents
+from src.models import make_texts_corpus
+from utils import editorJs_data_to_text
 
 client = pymongo.MongoClient(settings.MONGODB_SETTINGS["host"])
 db = client[settings.MONGODB_SETTINGS["db"]]
@@ -17,23 +17,81 @@ mongo_col = db[settings.MONGODB_SETTINGS["collection"]]
 app = Flask(__name__)
 api = Api(app)
 
+
+def load_model():
+    import gensim  # noqa
+    import joblib  # noqa
+    # load LDA model
+    lda_model = gensim.models.LdaModel.load(
+        settings.PATH_LDA_MODEL
+    )
+    # load corpus
+    corpus = gensim.corpora.MmCorpus(
+        settings.PATH_CORPUS
+    )
+    # load dictionary
+    id2word = gensim.corpora.Dictionary.load(
+        settings.PATH_DICTIONARY
+    )
+    # load documents topic distribution matrix
+    doc_topic_dist = joblib.load(
+        settings.PATH_DOC_TOPIC_DIST
+    )
+    # doc_topic_dist = np.array([np.array(dist) for dist in doc_topic_dist])
+
+    return lda_model, corpus, id2word, doc_topic_dist
+
+
+lda_model, corpus, id2word, doc_topic_dist = load_model()
+
+
 class Post(Resource):
     def get(self, post_id):
         main_post = mongo_col.find_one({"_id": ObjectId(post_id)})
-        main_post= {
+        main_post = {
             "_id": str(main_post["_id"]),
-            "text": editorJsDataToText(json.loads(main_post["content"])),
+            "text": editorJs_data_to_text(json.loads(main_post["content"])),
             "title": main_post["title"],
             "subtitle": main_post["subtitle"],
             "content": main_post["content"]
-
         }
 
-        return jsonify({"post": main_post})
+        # preprocessing
+        content = editorJs_data_to_text(json.loads(main_post["content"]))
+        text_corpus = make_texts_corpus([content])
+        bow = id2word.doc2bow(next(text_corpus))
+        doc_distribution = np.array(
+            [doc_top[1] for doc_top in lda_model.get_document_topics(bow=bow)]
+        )
+
+        # recommender posts
+        most_sim_ids = list(get_most_similar_documents(
+            doc_distribution, doc_topic_dist))[1:]
+
+        most_sim_ids = [int(id_) for id_ in most_sim_ids]
+        posts = mongo_col.find({"idrs": {"$in": most_sim_ids}})
+        related_posts = [
+                            {
+                                "_id": str(post["_id"]),
+                                "title": post["title"],
+                            }
+                            for post in posts
+                        ][1:]
+
+        return jsonify({"post": {
+            "_id": str(main_post["_id"]),
+            "text": editorJs_data_to_text(json.loads(main_post["content"])),
+            "title": main_post["title"],
+            "subtitle": main_post["subtitle"]
+        },
+            "related_posts": related_posts,
+        })
+
 
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
+
 
 api.add_resource(HelloWorld, '/')
 api.add_resource(Post, '/posts/<post_id>')
