2e4359a8123b3256a7a56ac4eb40c577a112168e
Duc Nguyen <ntrungduc228@gmail.com>
2022-12-18 18:03:07 +0700

    get raw content vietnamese from post

---
+++ src/settings.py
@@ -0,0 +1,10 @@
+MONGODB_SETTINGS = {
+    'db': 'blog-nodv',
+    'collection': 'posts',
+    'host': 'mongodb+srv://nodv:nodv@blog-nodv.vbgx2gc.mongodb.net/?retryWrites=true&w=majority'
+}
+
+PATH_DICTIONARY = "../models/id2word.dictionary"
+PATH_CORPUS = "../models/corpus.mm"
+PATH_LDA_MODEL = "../models/LDA.model"
+PATH_DOC_TOPIC_DIST = "../models/doc_topic_dist.dat"
---
+++ src/utils.py
@@ -0,0 +1,102 @@
+import logging
+import re
+import os
+current_directory = os.getcwd()
+from bs4 import BeautifulSoup
+from pyvi import ViTokenizer
+from pyeditorjs import EditorJsParser
+
+logging.basicConfig(format='%(levelname)s : %(message)s', level=logging.INFO)
+logging.root.level = logging.INFO
+
+with open('../data/vni_stopwords.txt', encoding="utf8") as f:
+    stopwords = []
+    for line in f:
+        stopwords.append("_".join(line.strip().split()))
+
+
+def preprocessing_tags(soup, tags=None):
+    if tags is not None:
+        for tag in tags:
+            for sample in soup.find_all(tag):
+                sample.replaceWith('')
+    else:
+        raise NotImplementedError("Tags must be set!")
+
+    return soup.get_text()
+
+
+
+def editorJsDataToText(data, tags=['pre', 'code', 'a', 'img', 'i']):
+    """ Converts a editorjs data to plaintext
+    """
+    parser = EditorJsParser(data)
+    html = parser.html(sanitize=True)
+    soup = BeautifulSoup(html, 'html.parser')
+    # remove code snippets
+    text = preprocessing_tags(soup, tags)
+
+    text = remove_links_content(text)
+    text = remove_emails(text)
+    text = remove_punctuation(text)
+    text = text.replace('\n', ' ')
+    text = remove_numeric(text)
+    text = remove_multiple_space(text)
+    text = text.lower().strip()
+    text = ViTokenizer.tokenize(text)
+    text = remove_stopwords(text, stopwords=stopwords)
+
+    return text
+
+
+def remain_tags_space(text, tags_space):
+    """
+    :param tags_space: tag to remained
+        {
+            "ruby on rails": "ruby_on_rails",
+            ...
+        }
+    """
+    for tag in tags_space:
+        text = text.replace(tag, tags_space[tag])
+    return ['_'.join(tag.split()) for tag in tags_space]
+
+
+def remove_emails(text):
+    return re.sub('\S*@\S*\s?', '', text)
+
+
+def remove_newline_characters(text):
+    return re.sub('\s+', ' ', text)
+
+
+def remove_links_content(text):
+    text = re.sub(r"http\S+", "", text)
+    return text
+
+
+def remove_multiple_space(text):
+    return re.sub("\s\s+", " ", text)
+
+
+def remove_punctuation(text):
+    """https://stackoverflow.com/a/37221663"""
+    import string  # noqa
+    table = str.maketrans({key: None for key in string.punctuation})
+    return text.translate(table)
+
+
+def remove_numeric(text):
+    import string  # noqa
+    table = str.maketrans({key: None for key in string.digits})
+    return text.translate(table)
+
+
+def remove_html_tags(text):
+    """Remove html tags from a string"""
+    clean = re.compile('<.*?>')
+    return re.sub(clean, '', text)
+
+
+def remove_stopwords(text, stopwords):
+    return " ".join([word for word in text.split() if word not in stopwords])
