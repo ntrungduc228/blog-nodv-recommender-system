97f38ce477bc66eabc7a2e163f162ea9910e6481
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2022-12-28 22:08:54 +0700

    Update cors

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 from flask_restful import Resource, Api
 import numpy as np
 import pymongo
@@ -15,6 +16,7 @@ db = client[settings.MONGODB_SETTINGS["db"]]
 mongo_col = db[settings.MONGODB_SETTINGS["collection"]]
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 
@@ -89,58 +91,57 @@ class Post(Resource):
             "related_posts": related_posts,
         })
 
+
 class PostsRecommend(Resource):
-  def get(self, post_id):
-    main_post = mongo_col.find_one({"_id": ObjectId(post_id)}, {"content": 1})
-    # preprocessing
-    content = editorJs_data_to_text(json.loads(main_post["content"]))
-    text_corpus = make_texts_corpus([content])
-    bow = id2word.doc2bow(next(text_corpus))
-    doc_distribution = np.array(
-        [doc_top[1] for doc_top in lda_model.get_document_topics(bow=bow)]
-    )
-    # recommender posts
-    most_sim_ids = list(get_most_similar_documents(
-        doc_distribution, doc_topic_dist))[1:]
+    def get(self, post_id):
+        main_post = mongo_col.find_one({"_id": ObjectId(post_id)}, {"content": 1})
+        # preprocessing
+        content = editorJs_data_to_text(json.loads(main_post["content"]))
+        text_corpus = make_texts_corpus([content])
+        bow = id2word.doc2bow(next(text_corpus))
+        doc_distribution = np.array(
+            [doc_top[1] for doc_top in lda_model.get_document_topics(bow=bow)]
+        )
+        # recommender posts
+        most_sim_ids = list(get_most_similar_documents(
+            doc_distribution, doc_topic_dist))[1:]
 
-    most_sim_ids = [int(id_) for id_ in most_sim_ids]
-    posts = mongo_col.aggregate([
-      {"$lookup": {
-        "from": "users",
-        "localField": "user.$id",
-        "foreignField": "_id",
-        "pipeline": [
-          {"$project": {"_id": 0, "id": {
-            "$toString": "$_id"
-          }, "username": 1, "email": 1, "avatar": 1}}
-        ],
-        "as": "user"
-        }},
-      {"$match": {"idrs": {"$in": most_sim_ids}}},
-      {"$project": {"_id": 0,"id": {
-        "$toString": "$_id"
-      }, "title": 1, "subtitle": 1,"thumbnail" : 1, "user": {"$arrayElemAt": ["$user", 0]}, "createdDate":{
-        "$dateToString": {
-          "date": "$createdDate"
-        }
-      }}},
-      {"$sort": {"createdDate": -1}},
-      {"$limit": 10}
-    ])
+        most_sim_ids = [int(id_) for id_ in most_sim_ids]
+        posts = mongo_col.aggregate([
+            {"$lookup": {
+                "from": "users",
+                "localField": "user.$id",
+                "foreignField": "_id",
+                "pipeline": [
+                    {"$project": {"_id": 0, "id": {
+                        "$toString": "$_id"
+                    }, "username": 1, "email": 1, "avatar": 1}}
+                ],
+                "as": "user"
+            }},
+            {"$match": {"idrs": {"$in": most_sim_ids}}},
+            {"$project": {"_id": 0, "id": {
+                "$toString": "$_id"
+            }, "title": 1, "subtitle": 1, "thumbnail": 1, "user": {"$arrayElemAt": ["$user", 0]}, "createdDate": {
+                "$dateToString": {
+                    "date": "$createdDate"
+                }
+            }}},
+            {"$sort": {"createdDate": -1}},
+            {"$limit": 10}
+        ])
 
-    related_posts = [
-        {
-            "id": post["id"],
-            "title": post["title"],
-            "user": post["user"],
-            "thumbnail": post["thumbnail"] if "thumbnail" in post else None,
-            "createdDate": post["createdDate"],
-        }
-        for post in posts
-    ][1:]
-    return jsonify(related_posts)
-
-
+        related_posts = [
+                            {
+                                "id": post["id"],
+                                "title": post["title"],
+                                "user": post["user"],
+                                "thumbnail": post["thumbnail"] if "thumbnail" in post else None,
+                                "createdDate": post["createdDate"],
+                            }
+                            for post in posts
+                        ][1:]
+        return jsonify(related_posts)
 
 
 api.add_resource(PostsRecommend, '/api/posts/<post_id>/recommend')
@@ -152,4 +153,4 @@ api.add_resource(Post, '/posts/<post_id>')
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host="localhost", port=8082, debug=True)
